f22ab35e5bf8b25a13ba78e862d5b7144514cbc6
Radium Zheng <ztl8702@msn.com>
2018-05-24 18:40:03 +1000

    [Refactor] MapManager

--- src/mycontroller/common/Cell.java
+++ src/mycontroller/common/Cell.java
@@ -1,57 +1,57 @@
 package mycontroller.common;
 
+/**
+ * Our internal representation of the MapTile
+ */
 public class Cell {
-	
-	public enum CellType {ROAD, WALL, START, FINISH, TRAP, UNREACHABLE}
+
+	public enum CellType {ROAD, WALL, START, FINISH, LAVA, HEALTH, UNREACHABLE}
 	public int key;
 	public CellType type;
-	
+
 	public String getCellTypeHash() {
 		return String.format("(%s,%d)",this.type.toString(),this.key);
 	}
-	
+
 	public static Cell newRoadCell() {
 		Cell newCell = new Cell();
 		newCell.type = CellType.ROAD;
 		newCell.key = 0;
 		return newCell;
 	}
-	
+
 	public static Cell newWallCell() {
 		Cell newCell = new Cell();
 		newCell.type = CellType.WALL;
 		newCell.key = 0;
 		return newCell;
 	}
-	
+
 	public static Cell newStartCell() {
 		Cell newCell = new Cell();
 		newCell.type = CellType.START;
 		newCell.key = 0;
 		return newCell;
 	}
-	
+
 	public static Cell newFinishCell() {
 		Cell newCell = new Cell();
 		newCell.type = CellType.FINISH;
 		newCell.key = 0;
 		return newCell;
 	}
-	
-	public static Cell newTrapCell(int key) {
+
+	public static Cell newLavaCell(int key) {
 		Cell newCell = new Cell();
-		newCell.type = CellType.TRAP;
+		newCell.type = CellType.LAVA;
 		newCell.key = key;
 		return newCell;
 	}
-	
-	// This method allows us to decide which node is better. 
-	// We can say current node is better if this method returns negative number,
-	// which means current node has lower cost than the other node being compared.
-	public static int compareTo(Cell currentCell, Cell otherCell) {
-//		if (currentCell.type == CellType.)
-//		TODO
-		return 1;
-		
+
+	public static Cell newHealthCell() {
+		Cell newCell = new Cell();
+		newCell.type = CellType.HEALTH;
+		newCell.key = 0;
+		return newCell;
 	}
 }
