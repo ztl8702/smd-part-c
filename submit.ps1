d18fcfa06f16d93af062923bfd1d3483f5141170
Radium Zheng <ztl8702@msn.com>
2018-05-27 20:21:05 +1000

    update submit script

--- submit.ps1
+++ submit.ps1
@@ -1,4 +1,6 @@
 # One-click submission creation
 
 # TODO: add diagram files
-& 'C:\Program Files\7-Zip\7z.exe' a submission.zip '.\src\mycontroller\' #.\report.pdf .\class_diagram.pdf 
+rm -Force .\submission.zip
+
+& 'C:\Program Files\7-Zip\7z.exe' a submission.zip '.\src\mycontroller\' '.\diagrams\design_rationale_report.pdf' '.\diagrams\class_diagram.pdf' '.\diagrams\communication_diagram.png' 
