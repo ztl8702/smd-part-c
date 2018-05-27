79ea0044c57fa5eceb5df429a2eaec3315fe3bf0
Radium <ztl8702@users.noreply.github.com>
2018-05-27 11:58:19 +1000

    Autopilot comments (#5)
    * [Refactor] AutoPilotFactory
    
    * fixed nullPointerEx when getCell
    
    * nulllllll
    
    * recentering works!
    
    * finally working astar
    
    * [Test Case] Edit the spiral map
    
    * [Test Case] Edit the spiral map
    
    * fixed direction issue
    
    * reset given code
    
    * fixed no key issue
    
    * clean up
    
    * bug with autocontroller
    
    * tuning auto pilot
    
    * modify map
    
    * tiny fixes in BaseAutoPilot
    
    * Rename BaseAutoPilot to AutoPilotBase
    
    * [Refactor] Utility class; fix a* bugs
    
    * minor changes to test astar (before sleeping)
    
    * still merging with radium/master
    
    * [AutoPilot] fix bug in centre line shifting
    
    * after merge with radium/master
    
    * [AutoPilot] fix bug in centre line shifting
    
    * [Test case] Fix Ziren's maps
    
    * [Refactor] Use logger to handle logging
    
    * [AutoPilot] Temporary fix for recentering
    
    * writing healthPathFinder
    
    * merge with temporary recentering fix
    
    * [AutoPilot] Wall buffer: treat map boundaries as Wall
    
    * before merging with radium/master for logger refactor
    
    * T/algo (#2)
    
    * minor changes to test astar (before sleeping)
    
    * still merging with radium/master
    
    * after merge with radium/master
    
    * [AutoPilot] fix bug in centre line shifting
    
    * writing healthPathFinder
    
    * merge with temporary recentering fix
    
    * before merging with radium/master for logger refactor
    
    * health path finder, without stopping until 100
    
    * debugging to see if health is workin
    
    * [Navigator] Allow interrupting
    
    * [MyAIController] use interrupt to stop exploring
    
    * [Refactor] Wrap getAStarPath in a new FinisherPathFinder
    
    * clean up
    
    * [PathFinder] Wrap getHealthPath in a class
    
    * [Test case] remove 1-width road in test-04.tmx
    
    * [PathFinder] Rewrite WallFollowingPathFinder
    
    * [PathFinder] Rewrite WallFollowingPathFinder yet again!
    Turns out we don't need DFS!
    
    * minor clean ups
    
    * [AutoPilot & RouteCompiler] let the car turn at speed 2
    
    - Except for width-2 U-turn and L-turns
    - There might be protential issues with ReCentreAutoPilot
    (recentring at a speed too high), but has not
    surfaced yet
    
    * added heuristic to favour tiles not beside a wall
    
    * [Refactor] Move a few constants and common functions to Util
    
    * [AutoPilot] TurningAutoPilot handles situation where
    desired turning speed cannot be reached
    
    * [Test case] Fix Ziren's map
    
    * [Navigator] Revise the stopping distance estimation
    
    Don't interrupt a path when there is no enough
    stopping distance ahead!
    
    * Fix IndexOutOfBound in AStarPathFinder
    
    * [Navigator] Don't interrupt on Lava tiles
    
    * [AutoPilot] TurningAutoPilot: Fix turning when the
    speed is too low.
    
    * [AutoPilot] Increase the recentering threshold a little bit
    
    * [RouteCompiler] Remove GoStraight segments with length = 1
    
    * [PathFinder] Change AStar Heuristics
    
    * Delete Part C.iml
    
    * Unbang / unstuck mode
    
    * fix unbanging logic
    
    * [PathFinder] WallFollowing: treat narrow openings on
    walls as walls.
    
    Otherwise it might produce invalid paths.
    
    * Delete ColoredRegion
    
    * Add comments to AutoPilot classes

--- src/mycontroller/autopilot/AutoPilotFactory.java
+++ src/mycontroller/autopilot/AutoPilotFactory.java
@@ -9,28 +9,69 @@ import mycontroller.mapmanager.MapManagerInterface;
 import utilities.Coordinate;
 import world.WorldSpatial;
 
+/**
+ * Pure fabrication that creates AutoPilot instances
+ *
+ * Because we need to inject mapManager into each AutoPilot
+ */
 public class AutoPilotFactory {
 
     private static MapManagerInterface mapManager;
 
+    /**
+     * Saves the reference to MapManager, so that it can be injected later
+     * @param mapManager
+     */
     public static void initialise(MapManagerInterface mapManager) {
         AutoPilotFactory.mapManager = mapManager;
     }
 
+    /**
+     * Creates a ForwardToAutoPilot
+     * @param start
+     * @param finish
+     * @param speed
+     * @return
+     */
     public static AutoPilot forwardTo(Coordinate start, Coordinate finish, float speed) {
         return new ForwardToAutoPilot(mapManager, start, finish, speed);
     }
 
+    /**
+     * Creates a TurningAutoPilot
+     * @param start
+     * @param end
+     * @param whichWayToTurn
+     * @param speed
+     * @return
+     */
     public static AutoPilot turn(Coordinate start, Coordinate end, WorldSpatial.RelativeDirection whichWayToTurn,
             float speed) {
         return new TurningAutoPilot(mapManager, start, end, whichWayToTurn, speed);
     }
 
+    /**
+     * Creates a MaintainSpeedAutoPilot
+     * @param speed
+     * @return
+     */
     public static AutoPilot maintainSpeed(float speed) {
         return new MaintainSpeedAutoPilot(mapManager, speed);
     }
 
+    /**
+     * Creates an EmergencyStopAutoPilot
+     * @return
+     */
     public static AutoPilot stop() {
         return new EmergencyStopAutoPilot(mapManager);
+    }
+
+    /**
+     * Creates a RecentreAutoPilot
+     */
+
+    public static AutoPilot recentre(ReCentreAutoPilot.CentringAxis axis, float newCentreLine) {
+        return new ReCentreAutoPilot(mapManager, axis, newCentreLine);
     }
 }
